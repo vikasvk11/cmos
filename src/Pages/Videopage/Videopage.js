c8878aefbdd2bae5dba4fcbea71cc3c7c7e7d9d0
vikasvk11 <74751412+vikasvk11@users.noreply.github.com>
2021-05-30 23:00:26 +0530

    add: basic video description

--- src/Pages/Videopage/Videopage.js
+++ src/Pages/Videopage/Videopage.js
@@ -5,7 +5,7 @@ import {usePlaylist} from "../../Context/PlaylistProvider.js";
 import "../../styles.css";
 import "./videopage.css";
 import {MenuList} from "../../Components/MenuList";
-import { Thumbnail } from "../../Components/Thumbnail";
+import {Thumbnail} from "../../Components/Thumbnail";
 
 export function Videopage() {
     const {videoId} = useParams();
@@ -36,31 +36,33 @@ export function Videopage() {
 
     useEffect(() => {
         window.scrollTo(0, 0)
-      }, [])
+    }, [])
 
-    const nextVideos = data.filter(item => item.id !== videoId);
+    const nextVideos = data
+        .filter(item => item.id !== videoId)
+        .slice(0, 6);
 
     shuffleArray(nextVideos);
 
     return ( 
         <> 
         <div className="videopage-main-container">
-            <MenuList/>
-            <div className="videopage-container_main">
-                <div className="videopage-container">
-                    <div className="video-wrapper">
-                        <iframe
-                            className="video"
-                            src={`https://www.youtube.com/embed/${videoId}`}
-                            title="YouTube video player"
-                            frameBorder="0"
-                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                            allowFullScreen></iframe>
-                    </div>
-                    <h1 className="videopage-header">{title}</h1>
-                    <div className="videoactions-bar">
-                        <p className="videoactions-bar_views">1,200,000 views</p>
-                        <div className="videoactions-bar_btns">
+        <MenuList/>
+        <div className="videopage-container_main">
+            <div className="videopage-container">
+                <div className="video-wrapper">
+                    <iframe
+                        className="video"
+                        src={`https://www.youtube.com/embed/${videoId}`}
+                        title="YouTube video player"
+                        frameBorder="0"
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                        allowFullScreen></iframe>
+                </div>
+                <h1 className="videopage-header">{title}</h1>
+                <div className="videoactions-bar">
+                    <p className="videoactions-bar_views">1,200,000 views</p>
+                    <div className="videoactions-bar_btns">
                         <button
                             className="btn-primary-outline mg_1 videopage-btn"
                             onClick={() => playlistDispatch({
@@ -77,27 +79,49 @@ export function Videopage() {
                                     <span className="material-icons f5">thumb_up_off_alt</span>
                                 )}
                         </button>
-                        <button className="btn-primary-outline videopage-btn" onClick={() => setModalState(true)}>
+                        <button
+                            className="btn-primary-outline videopage-btn"
+                            onClick={() => setModalState(true)}>
                             <span className="material-icons f5">playlist_add</span>
                         </button>
-                        </div>
-                    </div>       
+                    </div>
                 </div>
             </div>
-            <div className="videopage-morevideos-container">
-                <h2 className="videopage-morevideos_header">More Videos</h2>
-                <ul className="video-list-container mgt1">
-                    {nextVideos.map(videoItem => {
-                        return (
-                            <Thumbnail key={videoItem.id} videoId={videoItem.id} videoTitle={videoItem.title}/>
-                        );
-                    })}
-                </ul>
+            <div className="video-description-container">
+                <div className="video-description_header-container">
+                    <h1 className="video-description_header">Description</h1>
+                    <span className="material-icons">expand_more</span>
+                </div>
+                <p className="video-description_text pop">
+                    Today we're looking at comparing a $1,000.00 camera VS an $8,000.00 camera.
+                    Specifically, the 80D vs the 1DXMK2. I have seen the 80D on sale as low as
+                    999.99 which is why I went with that comparison price. Keep currency conversions
+                    in mind. 2 MASSIVELY different cameras. 
+                    You may be saying, why would you compare
+                    2 cameras on such a drastically different scale? That makes no sense and no one
+                    would ever compare those two - Yes and No. I wanted to go over a few of the
+                    features that MAKE a camera 8,000.00 opposed to those that don't. The quality
+                    difference is also incredibly evident.
+                </p>
             </div>
-        </div> 
-    
-    <div className = {`modal-bg ${modalState? "modal-bg-active": ""}`}> 
-      <div className="modal">
+        </div>
+        <div className="videopage-morevideos-container">
+            <h2 className="videopage-morevideos_header">More Videos</h2>
+            <ul className="video-list-container mgt1">
+                {nextVideos.map(videoItem => {
+                    return (<Thumbnail
+                        key={videoItem.id}
+                        videoId={videoItem.id}
+                        videoTitle={videoItem.title}/>);
+                })}
+            </ul>
+        </div>
+    </div> 
+    <div className = {
+        `modal-bg ${modalState
+            ? "modal-bg-active"
+            : ""}`
+    } > <div className="modal">
         <div className="modal-head">
             <h1 className="modal-header">Add to playlist</h1>
             <span
@@ -145,8 +169,7 @@ export function Videopage() {
                 <span className="material-icons">add</span>
             </button>
         </div>
-      </div> 
-      </div>
-    </>
-    );
+    </div> 
+    </div>
+    </>);
 }
