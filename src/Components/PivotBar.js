1e5892548c4f4678cb5056d65c3bfad736a94403
vikasvk11 <74751412+vikasvk11@users.noreply.github.com>
2021-09-26 15:52:46 +0530

    add

--- src/Components/PivotBar.js
+++ src/Components/PivotBar.js
@@ -14,25 +14,25 @@ export function PivotBar() {
             <li onClick={() => {
               navigate("/");
               setState([true, false, false, false]);
-              }} className={`nav-list-item ${state[0] ? "active-item" : "" }`}>
+              }} className={`nav-list-item pivot-bar-item ${state[0] ? "active-item" : "" }`}>
               <span className="material-icons">home</span>
             </li>
             <li onClick={() => {
               navigate("/liked");
               setState([false, true, false, false]);
-              }} className={`nav-list-item ${state[1] ? "active-item" : "" }`}>
+              }} className={`nav-list-item pivot-bar-item ${state[1] ? "active-item" : "" }`}>
               <span className="material-icons thumb_up">thumb_up</span>
             </li>
             <li onClick={() => {
               navigate("/playlists");
               setState([false, false, true, false]);
-              }} className={`nav-list-item ${state[2] ? "active-item" : "" }`}>
+              }} className={`nav-list-item pivot-bar-item ${state[2] ? "active-item" : "" }`}>
               <span className="material-icons">playlist_play</span>
             </li>
             <li onClick={() => {
               navigate("/history");
               setState([false, false, false, true]);
-              }} className={`nav-list-item ${state[3] ? "active-item" : "" }`}>
+              }} className={`nav-list-item pivot-bar-item ${state[3] ? "active-item" : "" }`}>
               <span className="material-icons history-icon">history</span>
             </li>
           </ul>
